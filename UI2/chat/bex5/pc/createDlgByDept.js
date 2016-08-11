39238cbcc3c921e96de2fc23a719a6ad792698af
rdc-justep <song@justep.com>
2016-08-11 18:36:58 +0800

    创建群组多次点击创建多个群组

--- UI2/chat/bex5/pc/createDlgByDept.js
+++ UI2/chat/bex5/pc/createDlgByDept.js
@@ -6,23 +6,35 @@ define(function(require) {
 	var Model = function() {
 		this.callParent();
 		this.deptName = justep.Bind.observable();
+		this.createGroupState = justep.Bind.observable(true);
 	};
 
 	Model.prototype.sendMessageBtnClick = function(event) {
 		var sFID = this.params.sFID;
 		var sName = this.params.sName;
-		justep.Util.confirm("确定将此部门及其子部门下所有人创建为群组？", function() {
-			IM.createDeptDlg(sFID, sName).done(function(peer) {
-				justep.Shell.fireEvent("onGroupSendMessagePage", {
-					id : peer.id,
-					type : peer.type
+		var self = this;
+		if(this.createGroupState.get()){
+			this.createGroupState.set(false);
+			justep.Util.confirm("确定将此部门及其子部门下所有人创建为群组？", function() {
+				IM.createDeptDlg(sFID, sName).done(function(peer) {
+					justep.Shell.fireEvent("onGroupSendMessagePage", {
+						id : peer.id,
+						type : peer.type
+					});
+					self.createGroupState.set(true);
 				});
 			});
-		})
+		}else{
+			justep.Util.hint("正在创建群组请稍后");
+		}
 	};
 
 	Model.prototype.modelModelConstructDone = function(event) {
 		this.deptName.set(this.params.sName);
+	};
+
+	Model.prototype.modelInactive = function(event){
+		this.createGroupState.set(true);
 	};
 
 	return Model;
--- UI2/chat/wex5/pc/createDlgByDept.js
+++ UI2/chat/wex5/pc/createDlgByDept.js
@@ -6,23 +6,61 @@ define(function(require) {
 	var Model = function() {
 		this.callParent();
 		this.deptName = justep.Bind.observable();
+		this.userIds = [];
+		this.count = 0;
+		this.personNum = 0;
+		this.createGroupState = justep.Bind.observable(true);
 	};
 
 	Model.prototype.sendMessageBtnClick = function(event) {
 		var sFID = this.params.sFID;
 		var sName = this.params.sName;
-		justep.Util.confirm("确定将此部门及其子部门下所有人创建为群组？", function() {
-			IM.createDeptDlg(sFID, sName).done(function(peer) {
-				justep.Shell.fireEvent("onGroupSendMessagePage", {
-					id : peer.id,
-					type : peer.type
+		var self = this;
+		if(this.createGroupState.get()){
+			IM.getOrgPersons(IM.getCurrentPersonID()).done(function(persons) {
+				var rows = [];
+				$.each(persons, function(i, v) {
+					if (v.sFID.indexOf(sFID) === 0&&v.sPersonID!==IM.getCurrentPersonID()) {
+						rows.push(IM.getPerson(v.sPersonID));
+					}
 				});
+				self.personNum = rows.length;
+				if (rows.length >= 0) {
+					justep.Util.confirm("确定将此部门及其子部门下所有人创建为群组？", function() {
+						self.createGroupState.set(false);
+						var addPerson = function(uid) {
+							self.userIds.push(uid);
+							self.count++;
+							if (self.count == self.personNum) {
+								IM.createGroup(sName, null, self.userIds).then(function(peer) {
+									justep.Shell.fireEvent("onGroupSendMessagePage", {
+										id : peer.id,
+										type : peer.type
+									});
+									self.createGroupState.set(true);
+								}, function(e) {
+									self.createGroupState = false;
+									justep.Util.hint("创建失败！");
+								});
+							}
+						};
+						for (var i = 0; i < rows.length; i++) {
+							IM.regPerson(rows[i]).done(addPerson.bind());
+						}
+					});
+				}
 			});
-		});
+		}else{
+			justep.Util.hint("正在创建群组请稍后...");
+		}
 	};
 
 	Model.prototype.modelModelConstructDone = function(event) {
 		this.deptName.set(this.params.sName);
+	};
+
+	Model.prototype.modelInactive = function(event){
+		this.createGroupState.set(true);
 	};
 
 	return Model;
